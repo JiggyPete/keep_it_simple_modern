b807b73753943a2e8d97b0d7ef4bfb2745e95d9e
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-29 07:40:21 +0000

    passing two instances of pair inspiration chooses may

--- lib/author.rb
+++ lib/author.rb
@@ -5,7 +5,10 @@ class Author
 
 	def new_best_seller_from( key_pair )
 		return "" if @inspiration.split.length < 3
-		remaining_text = @inspiration.split(key_pair)[1]  # need to make this smarter rather than using [1]
-	 	"#{key_pair}#{remaining_text}"
+
+		phrases = @inspiration.split(key_pair)
+		phrases.shift
+		remaining_text = phrases.join(key_pair)
+		"#{key_pair}#{remaining_text}"
 	end
 end

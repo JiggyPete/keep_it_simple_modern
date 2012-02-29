665f9b72ef14e1d0e5ff809d6c54151ba60eadf8
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-29 07:20:17 +0000

    passing suffers from writers block when given 2 words as inspiration

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe Author do 
-	
+
 	it "suffers from writers block when given blank" do 	
 		author = Author.new("")
 		author.new_best_seller_from("").should == ''
 	end
+
+	it "suffers from writers block when given 1 word as inspiration" do 	
+		author = Author.new("I")
+		author.new_best_seller_from("").should == ''
+	end
+
+	it "suffers from writers block when given 2 words as inspiration" do 	
+		author = Author.new("I wish")
+		author.new_best_seller_from("").should == ''
+	end
+
+
 
 end
